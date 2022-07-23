aa3f15f3d5221d950786c8f28491474ac3ec9f02
JOOHAPARK <umin110@naver.com>
2022-07-23 17:36:43 +0900

    Feat: add movie model

--- modelproject/movie/models.py
+++ modelproject/movie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title_kor = models.CharField(max_length=100)
+    title_eng = models.CharField(max_length=100)
+    poster_url = models.URLField()
+    rating_aud = models.FloatField()
+    rating_cri = models.FloatField()
+    rating_net = models.FloatField()
+    genre = models.CharField(max_length=50)
+    showtimes = models.CharField(max_length=10)
+    release_date = models.CharField(max_length=50)
+    rate = models.CharField(max_length=100)
+    summary = models.TextField()
+
